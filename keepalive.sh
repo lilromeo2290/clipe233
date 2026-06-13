a96a44b54c387b019065afb187859642449400b6
Clipe233 Engineers <clipe233@github.com>
2026-06-13 14:57:20 +0000

    1d2c410d-89cc-413e-ab7a-28ddc59234c2

--- keepalive.sh
+++ keepalive.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+cd /home/z/my-project
 while true; do
-  cd /home/z/my-project
-  npx next dev -p 3000
-  echo "Server died, restarting in 3s..."
+  echo "[$(date)] Starting server..." >> /tmp/nextalive.log
+  npx next dev -p 3000 >> /tmp/nextalive.log 2>&1
+  EXIT_CODE=$?
+  echo "[$(date)] Server exited with code $EXIT_CODE, restarting in 3s..." >> /tmp/nextalive.log
   sleep 3
 done
